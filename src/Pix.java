a2c6e76160f723687ca71c1710e5f3b1f5d717f1
Allan Lincoln <allanlincoln7@gmail.com>
2023-06-08 15:49:59 -0300

    first commit

---
+++ src/Pix.java
@@ -0,0 +1,6 @@
+public class Pix implements PaymentMethod {
+    public void efetuarPagamento(double valor) {
+
+        System.out.println("Pagamento via Pix no valor de R$" + valor + " realizado com sucesso.");
+    }
+}
